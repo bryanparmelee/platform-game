83fc3196f294b34f39d64a2ebcc98fdf962286ff
bryanparmelee <bryan.parmelee@gmail.com>
2023-03-09 22:06:43 -0500

    Add level grid

--- script.js
+++ script.js
@@ -127,3 +127,45 @@ const levelChars = {
   "|": "Lava",
   v: "Lava",
 };
+
+function elt(name, attrs, ...children) {
+  let dom = document.createElement(name);
+  for (let attr of Object.keys(attrs)) {
+    dom.setAttribute(attr, attrs[attr]);
+  }
+  for (let child of children) {
+    dom.appendChild(child);
+  }
+  return dom;
+}
+
+class DOMDisplay {
+  constructor(parent, level) {
+    this.dom = elt("div", { class: "game" }, drawGrid(level));
+    this.actorLayer = null;
+    parent.appendChild(this.dom);
+  }
+
+  clear() {
+    this.dom.remove();
+  }
+}
+
+const scale = 20;
+
+function drawGrid(level) {
+  return elt(
+    "table",
+    {
+      class: "background",
+      style: `width: ${(level.width = scale)}px`,
+    },
+    ...level.rows.map((row) =>
+      elt(
+        "tr",
+        { style: `height: ${scale}px` },
+        ...row.map((type) => elt("td", { class: type }))
+      )
+    )
+  );
+}
